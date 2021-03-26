4b892fcd411fdf7a82bba0b857d7e74287311810
devnazmul <md.nazmul.islam1332@gmail.com>
2021-03-26 23:52:54 +0600

    Create Loading Screen Ui

---
+++ app/components/Screen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
+import Constants from 'expo-constants'
+
+
+const Screen = ({ children }) => {
+    return (
+        <SafeAreaView style = {styles.screen} >
+            {children}
+        </SafeAreaView>
+    )
+}
+
+export default Screen
+
+const styles = StyleSheet.create({
+    screen: {
+        paddingTop: Constants.statusBarHeight,
+        flex: 1,
+    }
+})
--- app/screens/LoadingScreen.js
+++ app/screens/LoadingScreen.js
@@ -1,14 +1,54 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, ImageBackground, View, Image, Text } from 'react-native'
+
+import Screen from '../components/Screen'
+
 
 const LoadingScreen = () => {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <ImageBackground
+            style={styles.ImageBackground}
+            source={require("../assets/loading.jpg")}>
+            <Screen style={{}}>
+                <View style={styles.logoContainer}>
+                    <Image style={styles.logo} source={require("../assets/logo.png")} />
+                    <Text style={styles.Title} >Amazon</Text>
+                </View>
+
+                <View style={styles.loadingContainer}>
+                    <Image style={styles.loading} source={require("../assets/loading.gif")} />
+                </View>
+            </Screen>
+        </ImageBackground>
     )
 }
 
 export default LoadingScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    ImageBackground: {
+        flex: 1,
+    },
+    logo: {
+        width: 150,
+        height: 150,
+    },
+    logoContainer: {
+        alignItems: 'center',
+        flex:5
+    },
+    loading: {
+        height:80,
+        width:80
+    },
+    loadingContainer: {
+        alignItems: 'center',
+        flex:1
+    },
+    Title: {
+        fontSize: 50,
+        color: '#0d2636',
+        fontStyle: 'italic',
+        fontWeight: 'bold'
+    }
+})
